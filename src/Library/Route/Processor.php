d6c4e04e19f206379569a8d38775b84de1fff3da
Rishi Satsangi <rishi.satsangi@gmail.com>
2015-08-06 12:31:04 -0400

    validation is working

--- src/Library/Route/Processor.php
+++ src/Library/Route/Processor.php
@@ -1,6 +1,8 @@
 <?php
 
 require_once("Library/Route/Methods.php");
+require_once("Library/Exception/Route/MissingQueryParameterException.php");
+require_once("Library/Exception/Route/MissingBodyException.php");
 use Slim\Helper\Set;
 
 class Processor
@@ -20,17 +22,43 @@ class Processor
         //trim the leading slash
         $dirname = ltrim ($pathinfo['dirname'], '/');
         //replace slashes with underscores and append basename
-        $method = $dirname ? str_replace("/", "_", $dirname) . "_" . $pathinfo['basename'] : $pathinfo['basename'];
+        $method = strtolower($route['type']) . "_" . ($dirname ? str_replace("/", "_", $dirname) . "_" . $pathinfo['basename'] : $pathinfo['basename']);
 
-        
         $methods = new Methods($appContainer, $route);
         // check if example response is requested
         if ($this->appContainer['request']->headers->get('Example-Response-Body')) {
             $this->response = $this->getExampleResponseBody($this->appContainer['request']->headers->get('Example-Response-Code'));
             $appContainer['response']->setStatus($this->appContainer['request']->headers->get('Example-Response-Code'));
         } else {
+            $this->validateRequest();
             $this->response = $methods->$method();
         }
+    }
+
+    private function validateRequest()
+    {
+        foreach ($this->route['method']->getQueryParameters() as $namedParameter) {
+            if( $namedParameter->isRequired() ){
+                if (!in_array($namedParameter->getKey(), array_keys($this->appContainer['request']->params()))) {
+                    throw new MissingQueryParameterException($namedParameter->getKey() . ": ". $namedParameter->getDescription());
+                }   
+            }
+        }
+
+        $schema = null;
+        try {
+            $schema = $this->route['method']->getBodyByType('application/json')->getSchema();
+        } catch (Exception $e) {
+        }
+
+        if(!is_null($schema)){
+            if($schema->getJsonObject()->required) {
+                if ($this->appContainer['request']->getBody()=='') {
+                    throw new MissingBodyException($schema->__toString());
+                }
+            }    
+        }
+        
     }
 
     private function getExampleResponseBody($responseCode = 200)
