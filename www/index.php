58d9d7489e54fcbd6adb16eeaac5fa15c4508b77
Jaroslav Povolný <jaroslav.povolny@gmail.com>
2016-06-07 11:50:39 +0200

    added RobotLoader, removed require(''), path handling

--- www/index.php
+++ www/index.php
@@ -1,24 +1,35 @@
 <?php
+define('APPLICATION_PATH', __DIR__ . "/..");
 
-require '../vendor/autoload.php';
+require APPLICATION_PATH . '/vendor/autoload.php';
 
 \Tracy\Debugger::enable();
 
-define("APPLICATION_PATH", __DIR__ . "/..");
+//Robotloader
 
-// Ensure src/ is on include_path
-set_include_path(implode(PATH_SEPARATOR, array(
-    APPLICATION_PATH ,
-    APPLICATION_PATH . '/src',
-    get_include_path(),
-)));
+$loader = new Nette\Loaders\RobotLoader;
+$loader->addDirectory(APPLICATION_PATH . '/src');
+$loader->addDirectory(APPLICATION_PATH . '/controllers');
+$loader->setCacheStorage(new Nette\Caching\Storages\FileStorage(APPLICATION_PATH . '/temp'));
+$loader->register();
 
 use Symfony\Component\Yaml\Yaml;
-require_once("Library/Route/Processor.php");
-require_once('Library/Route/Controller.php');
+
 
 // Load configs and add to the app container
-$app = new \Slim\Slim();
+$app = new \Slim\Slim([
+    'mode' => 'production',
+]);
+
+// Only invoked if mode is "production"
+$app->configureMode('production', function () use ($app) {
+    $app->config(array(
+        'log.enable' => true,
+        'debug' => true
+    ));
+});
+
+
 $configs = Yaml::parse(file_get_contents("../configs/configs.yml"));
 $app->container->set('configs', $configs);
 
@@ -32,7 +43,8 @@ if (!$version) {
 
 // parse configured RAML and add api definition to app container
 $parser = new \Raml\Parser();
-$apiDef = $parser->parseFromString("raml/" . $version . "/" . $configs['api_name'] . ".raml", "/");
+
+$apiDef = $parser->parseFromString("raml/" . $version . "/" . $configs['api_name'] . ".raml", __DIR__ . "/");
 $app->container->set('apiDef', $apiDef);
 
 // This is where a persistence layer ACL check would happen on authentication-related HTTP request items
