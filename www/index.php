c090bf79128abd69c69b29c97f1ef8f923935590
jasir <jaroslav.povolny@gmail.com>
2016-06-08 23:58:31 +0200

    Refactoring: Process - no work should be done in constructor

--- www/index.php
+++ www/index.php
@@ -5,7 +5,7 @@ require APPLICATION_PATH . '/vendor/autoload.php';
 
 \Tracy\Debugger::enable();
 
-//Robotloader
+//RobotLoader
 
 $loader = new Nette\Loaders\RobotLoader;
 $loader->addDirectory(APPLICATION_PATH . '/src');
@@ -13,11 +13,11 @@ $loader->addDirectory(APPLICATION_PATH . '/controllers');
 $loader->setCacheStorage(new Nette\Caching\Storages\FileStorage(APPLICATION_PATH . '/temp'));
 $loader->register();
 
+use Slim\Slim;
 use Symfony\Component\Yaml\Yaml;
 
-
 // Load configs and add to the app container
-$app = new \Slim\Slim([
+$app = new Slim([
     'mode' => 'production',
 ]);
 
@@ -29,12 +29,12 @@ $app->configureMode('production', function () use ($app) {
     ));
 });
 
-
 $configs = Yaml::parse(file_get_contents("../configs/configs.yml"));
 $app->container->set('configs', $configs);
 
 // parse version from request uri
 $version = explode("/", ltrim($_SERVER['REQUEST_URI'], "/"))[0];
+
 if (!$version) {
     header('Content-Type: text/markdown');
     echo file_get_contents("../README.md");
@@ -42,21 +42,19 @@ if (!$version) {
 }
 
 // parse configured RAML and add api definition to app container
-$parser = new \Raml\Parser();
 
 $ramlDir = __DIR__ . "/raml/{$version}";
 $ramlPath = "/{$configs['api_name']}.raml";
-
 $source = file_get_contents($ramlDir . '/' . $ramlPath);
 
-
+$parser = new \Raml\Parser();
 $apiDef = $parser->parseFromString($source, $ramlDir);
 
 
 $app->container->set('apiDef', $apiDef);
 
 // This is where a persistence layer ACL check would happen on authentication-related HTTP request items
-$authenticate = function ($app) {
+$authenticate = function (Slim $app) {
     return function () use ($app) {
         if (false) {
             $app->halt(403, "Invalid security context");
@@ -65,15 +63,31 @@ $authenticate = function ($app) {
 };
 
 // Loop through the routes and register the API endpoints with the app
+
 foreach ($apiDef->getResourcesAsUri()->getRoutes() as $route) {
 
-    $type = strtolower($route['method']->getType());
-    $app->$type("/" . $apiDef->getVersion() . $route['path'], $authenticate($app), function () use ($app, $route) {
-        // Process the route
-        $routeProcessor = new Processor($app->container, $route);
-        // API definitions are assumed to have this Content-Type for all content returned
-        $app->response->headers->set('Content-Type', 'application/json');
-    });
+    $httpMethod = strtolower($route['method']->getType());
+
+    //get,post,...
+    $app->$httpMethod(
+
+        //route path
+        '/' . $apiDef->getVersion() . $route['path'],
+
+        //authenticate middleware
+        $authenticate($app),
+
+        //last middleware
+        function () use ($app, $route) {
+
+            // Process the route
+            $routeProcessor = new Processor($app->container, $route);
+            $routeProcessor->process();
+
+            // API definitions are assumed to have this Content-Type for all content returned
+            $app->response->headers->set('Content-Type', 'application/json');
+        }
+    );
 
 }
 
