e9d4cdf0ae03c31029d42cf9e7aab01a414a9234
jasir <jaroslav.povolny@gmail.com>
2016-07-01 18:23:03 +0200

    tests: bootstrap

--- tests/tests-bootstrap.php
+++ tests/tests-bootstrap.php
@@ -1,15 +1,21 @@
 <?php
 
+use Tracy\Debugger;
+
 define('APPLICATION_PATH', __DIR__ . "/..");
 
 require APPLICATION_PATH . '/vendor/autoload.php';
 
-\Tracy\Debugger::enable();
+Debugger::enable(Debugger::DEBUG, __DIR__);
+
 
 //RobotLoader
 
 $loader = new Nette\Loaders\RobotLoader;
 $loader->addDirectory(APPLICATION_PATH . '/src');
-$loader->addDirectory(APPLICATION_PATH . '/controllers');
+$loader->addDirectory(APPLICATION_PATH . '/tests');
+
+//$loader->addDirectory(APPLICATION_PATH . '/controllers');
 $loader->setCacheStorage(new Nette\Caching\Storages\FileStorage(__DIR__ . '/temp'));
 $loader->register();
+
